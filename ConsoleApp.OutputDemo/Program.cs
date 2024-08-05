b59833be32d5c36e56cac351b7649842587f36ea
jMeSpellman <jatrill@spellmanhv.co.uk>
2024-08-05 13:05:00 +0100

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is: ");
+Console.WriteLine("I am from UK!");
+
---
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿// Declare Variables
+
+string name = string.Empty;
+int age = 0;
+int retirementAge = 67;
+
+
+// Prompt the user for input
+Console.WriteLine("Please enter your name: ");
+name = Console.ReadLine();
+Console.WriteLine("Please enter your age: ");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Process data
+int workingYearsRemaining = retirementAge - age;
+
+// Output the results to the user
+Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Working Years remaining: {workingYearsRemaining}");
+
