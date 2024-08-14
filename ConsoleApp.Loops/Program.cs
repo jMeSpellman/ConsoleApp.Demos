0677295f32cc43e152251b6be45f8e11fdc9f7bd
jMeSpellman <jatrill@spellmanhv.co.uk>
2024-08-14 13:51:12 +0100

    Added Loop Demos and modified Calculator

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Simple For Loop Demo
+// Print Hello World 10 times
+
+/*
+Console.WriteLine("********** Simple For Loop **********");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Hello World! [{i}]");
+}
+
+// Ask User how many time they wish to Print Hello World and Print accordingly
+Console.WriteLine("How many times do you want to Print 'Hello World'");
+int copies = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; (i <= copies); i++)
+{
+    Console.WriteLine($"Hello World! [{i}]");
+}
+
+Console.WriteLine("********** Simple For Loop End **********");
+*/
+
+Console.WriteLine("********** While Loop **********");
+
+int num = 0;
+int total = 0;
+while(num != -1)
+{
+    Console.WriteLine("Please enter a number to be summed (-1 to stop or exit): ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if(num != -1){ total += num; }
+}
+
+Console.WriteLine($"You sum is: {total}");
+
+Console.WriteLine("********** While Loop End **********");
+
+Console.WriteLine("********** Do While Loop **********");
+
+num = 0;
+total = 0;
+do
+{
+    Console.WriteLine("Please enter a number to be summed (-1 to stop or exit): ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num != -1) { total += num; }
+
+} while (num != -1);
+
+Console.WriteLine($"You sum is: {total}");
+
+Console.WriteLine("********** Do While Loop End **********");
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,51 +1,81 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // Welcome Message   
-Console.WriteLine("Welcome to the Sample Calculator");
+Console.WriteLine("********** Welcome to the Sample Calculator **********");
 
-// Prompt the user for Input
-Console.WriteLine("Please Enter the first number:");
-int num1 = Convert.ToInt32(Console.ReadLine());
+int choice = 0;
 
-Console.WriteLine("Please Enter the second number:");
-int num2 = Convert.ToInt32(Console.ReadLine());
+    // Show caluclator options /Show menu
+    Console.WriteLine("Please select an Operation: (-1 to exit)");
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Fibonacci Sequence");
 
-// Show Calcultator Options
-Console.WriteLine("Please Select One of the Following:");
-Console.WriteLine(" + (Addition)");
-Console.WriteLine(" - (Subtraction)");
-Console.WriteLine(" * (Mulitplication)");
-Console.WriteLine(" / (Division)");
-Console.WriteLine(" % (Modulus)");
+    choice = Convert.ToInt32(Console.ReadLine());
 
-string? selection = Console.ReadLine();
-int result = 0;
-Boolean isValid = true;
 
-// Decide which opertion is needed based on selection
-switch (selection)
-{
-    case "+":
-        result = num1 + num2; break;
-    case "-":
-        result = num1 - num2; break;
-    case "*":
-        result = num1 * num2; break;
-    case "/":
-        result = num1 / num2; break;
-    case "%":
-        result = num1 % num2; break;
-    default:
-        isValid = false; 
-        break;
+while (choice != -1)
+{ 
+    // Prompt the user for Input
+    Console.WriteLine("Please Enter the first number:");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Please Enter the second number:");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Boolean isValid = true;
+    int answer = 0;
+    switch (choice)
+    {
+        case 1:
+            answer = (num1 + num2);
+            break;
+        case 2:
+            answer = (num1 - num2);
+            break;
+        case 3:
+            answer = (num1 * num2);
+            break;
+        case 4:
+            answer = (num1 / num2);
+            break;
+        case 5:
+            for (int i = num1; i <= num2; i++)
+            {
+                answer += i;
+            }
+            break;
+        default:
+            
+            isValid = false;
+            break;
+    }
+
+    if (isValid)
+    {
+        Console.WriteLine($"Result is: {answer}");
+        Console.WriteLine();
+        Console.WriteLine("Please press [ENTER] to continue");
+        Console.ReadLine();
+        Console.Clear();
+
+        // Show caluclator options /Show menu
+        Console.WriteLine("Please select an Operation: (-1 to exit)");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Fibonacci Sequence");
+
+        choice = Convert.ToInt32(Console.ReadLine());
+    }
+    else
+    {
+        choice = -1;
+        Console.WriteLine($"Invalid Operation");
+    }
 }
 
-// Print Ouput
-if (isValid)
-{
-    Console.WriteLine($"Result: {result}");
-}
-else
-{
-    Console.WriteLine($"Invalid Selection: {selection}");
-}
+Console.WriteLine("********** Thank you for using the Sample Calculator **********");
